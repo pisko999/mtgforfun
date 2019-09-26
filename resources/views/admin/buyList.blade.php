e247821b904eb474ff7a1f90f505cb5a702ce582
pisko999 <spinarp@gmail.com>
2019-09-26 11:30:50 +0200

    k

--- resources/views/admin/buyList.blade.php
+++ resources/views/admin/buyList.blade.php
@@ -42,8 +42,8 @@
                                             <tr>
                                                 <td style="width: 10%; text-align: center;">{{$card->number}}</td>
                                                 <td style="width: 80%; text-align: center;">{{$card->product->name}}</td>
-                                                <td style="width: 1%; text-align: center;">{{$card->quantity}}</td>
-                                                <td style="width: 1%; text-align: center;">{{count($card->product->stock) > 0?$card->product->stock->price:$card->product->price->MT}}</td>
+                                                <td style="width: 5%; text-align: center;">{{$card->quantity}}</td>
+                                                <td style="width: 5%; text-align: center;">{{count($card->product->stock) > 0?$card->product->stock->price:$card->product->base_price}}</td>
 
                                             </tr>
                                         </table>
